3a626844556938c005aba009722a09f660d0339d
Matt Moretti <werebus@gmail.com>
2016-06-24 16:14:08 -0400

    Goal scaffold

--- lib/pmp.rb
+++ lib/pmp.rb
@@ -3,6 +3,7 @@ Bundler.require(:default)
 
 require 'prawn/measurement_extensions'
 
+require 'pmp/goal'
 require 'pmp/planning_worksheet'
 require 'pmp/settings'
 
---
+++ lib/pmp/goal.rb
@@ -0,0 +1,22 @@
+module PMP
+  class Goal
+    FORMATTED_FIELDS = %i(description
+                          criteria
+                          employee_review
+                          supervisor_review).freeze
+    attr_accessor :due_date, :employee_review_date, :supervisor_review_date
+    attr_writer(*FORMATTED_FIELDS)
+
+    FORMATTED_FIELDS.each do |field|
+      define_method field do
+        format_text(instance_variable_get("@#{field}"))
+      end
+    end
+
+    private
+
+    def format_text(text)
+      text
+    end
+  end
+end
