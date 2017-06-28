e50532ffb64d4dda4be8c00024f74dd0eada8a29
Matt Moretti <werebus@gmail.com>
2017-06-28 18:11:55 -0400

    Rewrote Goal class
    Given that we're putting the PDF itself in charge of the markdown
    formatting, we don't need the formatted/unformatted distinction anymore.
    But, some handling of dates as dates would be nice since Psych parses
    them from YAML as `Date`s.
    
    Also, related to YAML-parsing, use the non-folding form of block text,
    so you can write "real" markdown in your yaml.

--- lib/pmp/goal.rb
+++ lib/pmp/goal.rb
@@ -1,23 +1,28 @@
 module PMP
   class Goal
-    FORMATTED_FIELDS = %i(description
-                          criteria
-                          employee_review
-                          supervisor_review).freeze
+    TEXT_FIELDS = %i[description criteria employee_review supervisor_review]
+    DATE_FIELDS = %i[due_date employee_review_date supervisor_review_date]
+    attr_accessor(*TEXT_FIELDS)
+    attr_reader(*DATE_FIELDS)
 
-    attr_accessor :due_date, :employee_review_date, :supervisor_review_date
-    attr_writer(*FORMATTED_FIELDS)
-
-    FORMATTED_FIELDS.each do |field|
-      define_method field do
-        format_text(instance_variable_get("@#{field}"))
+    DATE_FIELDS.each do |field|
+      define_method :"#{field}=" do |value|
+        case value
+        when Date, nil
+          instance_variable_set(:"@#{field}", value)
+        when String
+          instance_variable_set(:"@#{field}", Date.parse(value))
+        else
+          raise ArgumentError,
+                "#{field} must be a Date, parsable String, or nil"
+        end
       end
     end
 
-    private
-
-    def format_text(text)
-      text
+    def from_hash(document)
+      (TEXT_FIELDS + DATE_FIELDS).each do |field|
+        send(:"#{field}=", document.fetch(field.to_s))
+      end
     end
   end
 end
