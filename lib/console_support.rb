cb5ef8c2bc9ce51e78818c20707c68dbaebd60ac
Matt Moretti <werebus@gmail.com>
2017-06-29 16:06:47 -0400

    Use goals helper within the pw method

--- lib/console_support.rb
+++ lib/console_support.rb
@@ -1,12 +1,12 @@
 def pw
   load 'pmp/planning_worksheet.rb'
-  p=PMP::PlanningWorkSheet.new('John Doe', 'Example Position', Date.today..Date.today)
+  p=PMP::PlanningWorkSheet.new('John Doe', 'Example Position', Date.today..Date.today, goals)
   p.render_document
   p.save_as('out/example_planning_worksheet.pdf')
 end
 
 def goals
   File.open('goals.yml.example') do |f|
-    YAML.load_stream(f).map{|doc| PMP::Goal.from_hash(doc)}
+    YAML.load_stream(f).map{|doc| PMP::Goal.from_hash(doc)} * 5
   end
 end
