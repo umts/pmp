ff56c8ee65798933e619f3cd64d7c5afbade7215
Matt Moretti <werebus@gmail.com>
2017-07-06 11:18:23 -0400

    Delegate the dynamic setters to a private method
    Also document an example of the dynamic method definition. These are
    both inspired by reading over the style guide

--- lib/pmp/goal.rb
+++ lib/pmp/goal.rb
@@ -3,6 +3,7 @@ module PMP
     TEXT_FIELDS = %i[description criteria employee_review supervisor_review]
     DATE_FIELDS = %i[due_date employee_review_date supervisor_review_date]
     FIELDS = TEXT_FIELDS + DATE_FIELDS
+
     attr_accessor(*TEXT_FIELDS)
     attr_reader(*DATE_FIELDS)
 
@@ -11,17 +12,9 @@ module PMP
     end
 
     DATE_FIELDS.each do |field|
-      define_method :"#{field}=" do |value|
-        case value
-        when Date, nil
-          instance_variable_set(:"@#{field}", value)
-        when String
-          instance_variable_set(:"@#{field}", Date.parse(value))
-        else
-          raise ArgumentError,
-                "#{field} must be a Date, parsable String, or nil"
-        end
-      end
+      define_method :"#{field}=" do |value|                   #def due_date=(value)
+        instance_variable_set(:"@#{field}", date_cast(value)) #  @due_date = date_cast(value)
+      end                                                     #end
     end
 
     def from_hash(document)
@@ -29,6 +22,19 @@ module PMP
         send(:"#{field}=", document.fetch(field.to_s))
       end
       self
+    end
+
+    private
+
+    def date_cast(value)
+      case value
+      when Date, nil
+        value
+      when String
+        Date.parse(value)
+      else
+        raise ArgumentError, "#{field} must be a Date, parsable String, or nil"
+      end
     end
   end
 end
