3a9976c8e88a75047ddc3756435af122b939e711
Matt Moretti <werebus@gmail.com>
2021-07-08 15:24:03 -0400

    New console

--- lib/console_support.rb
+++ lib/console_support.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
-def pw
-  load 'pmp/planning_worksheet.rb'
-  p = PMP::PlanningWorksheet.new 'John Doe',
-                                 'Example Position',
-                                 Date.today..Date.today,
-                                 goals
-  p.render_document
-  p.save_as 'out/example_planning_worksheet.pdf'
-end
+module ConsoleSupport
+  def pw
+    load 'pmp/planning_worksheet.rb'
+    p = PMP::PlanningWorksheet.new 'John Doe',
+                                   'Example Position',
+                                   Date.today..Date.today,
+                                   goals
+    p.render_document
+    p.save_as 'out/example_planning_worksheet.pdf'
+  end
 
-def goals
-  File.open('goals.yml.example') do |f|
-    YAML.load_stream(f).map { |doc| PMP::Goal.from_hash(doc) } * 5
+  def goals
+    File.open('goals.yml.example') do |f|
+      YAML.load_stream(f).map { |doc| PMP::Goal.from_hash(doc) } * 5
+    end
   end
 end
