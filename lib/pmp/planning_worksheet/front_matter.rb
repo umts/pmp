8b4a8669d13ac799658ec75b1d30f1bce315a066
Matt Moretti <werebus@gmail.com>
2021-07-09 10:06:55 -0400

    Broke up front matter

--- lib/pmp/planning_worksheet/front_matter.rb
+++ lib/pmp/planning_worksheet/front_matter.rb
@@ -6,6 +6,43 @@ module PMP
       private
 
       def front_matter
+        %i[name_and_position guides review_dates ws_instructions
+           goal_expectations other_instructions].each do |document_part|
+          send document_part
+          move_down 6.pt
+        end
+      end
+
+      def goal_expectations
+        bounding_box([0, cursor], width: bounds.right) do
+          pad 4.pt do
+            bounding_box([4.pt, cursor], width: bounds.right - 4.pt) do
+              formatted_text [
+                { text: 'Every employee is expected to work on a ' },
+                { text: 'minimum of three goals and/or work priorities ', styles: [:bold] },
+                { text: 'and a ' },
+                { text: 'maximum of eight goals and or work priorities ', styles: [:bold] },
+                { text: 'during the review period.' }
+              ]
+            end
+          end
+          stroke_bounds
+        end
+      end
+
+      def guides
+        links = { 'PMP Handbook' => 'PMPHand.pdf', 'PMP Guidelines' => 'PMPGuide.pdf' }
+        links.each do |title, file|
+          formatted_text [
+            { text: "#{title} on Web  ", styles: [:italic] },
+            { text: "http://www.umass.edu/humres/#{file}",
+              color: '0000FF',
+              link: "http://www.umass.edu/humres/#{file}" }
+          ]
+        end
+      end
+
+      def name_and_position
         formatted_text [
           { text: 'EMPLOYEE NAME:', styles: [:bold] },
           { text: @name.center(20), styles: [:underline] },
@@ -13,22 +50,17 @@ module PMP
           { text: @position.center(30), styles: [:underline] },
           { text: Prawn::Text::NBSP }
         ]
-        move_down 6.pt
+      end
 
+      def other_instructions
+        text 'NOTE: Attach the Performance Planning Worksheet text to the annual review form.'
         formatted_text [
-          { text: 'PMP Handbook on Web  ', styles: [:italic] },
-          { text: 'http://www.umass.edu/humres/PMPHand.pdf',
-            color: '0000FF',
-            link: 'http://www.umass.edu/humres/PMPGuide.pdf' }
+          { text: 'Make additional copies if needed. ', styles: %i[bold italic] },
+          { text: 'For Electronic Users: Please adjust box sizes as data is entered.' }
         ]
-        formatted_text [
-          { text: 'PMP Guidelines on Web  ', styles: [:italic] },
-          { text: 'http://www.umass.edu/humres/PMPGuide.pdf',
-            color: '0000FF',
-            link: 'http://www.umass.edu/humres/PMPGuide.pdf' }
-        ]
-        move_down 6.pt
+      end
 
+      def review_dates
         formatted_text [
           { text: 'REVIEW PERIOD: From: ' },
           { text: @start_date.to_s.center(15), styles: [:underline] },
@@ -36,43 +68,14 @@ module PMP
           { text: @end_date.to_s.center(15), styles: [:underline] },
           { text: Prawn::Text::NBSP }
         ]
-        move_down 6.pt
+      end
 
-        formatted_text [{ text: <<~INST.gsub(/\n/, ''),
+      def ws_instructions
+        formatted_text [{ text: <<~INST.gsub(/\n/, ''), styles: [:bold] }], align: :center
           Use this worksheet to record goals/work priorities, specify the
           success criteria and, when completed, to comment on the end
           results.
         INST
-                          styles: [:bold] }],
-                       align: :center
-        move_down 6.pt
-
-        bounding_box([0, cursor], width: bounds.right) do
-          pad 4.pt do
-            bounding_box([4.pt, cursor], width: bounds.right - 4.pt) do
-              formatted_text [
-                { text: 'Every employee is expected to work on a ' },
-                { text: 'minimum of three goals and/or work priorities ',
-                  styles: [:bold] },
-                { text: 'and a ' },
-                { text: 'maximum of eight goals and or work priorities ',
-                  styles: [:bold] },
-                { text: 'during the review period.' }
-              ]
-            end
-          end
-          stroke_bounds
-        end
-        move_down 6.pt
-
-        note = 'NOTE: Attach the Performance Planning Worksheet' \
-               ' text to the annual review form.'
-        text note
-        formatted_text [
-          { text: 'Make additional copies if needed. ', styles: %i[bold italic] },
-          { text: 'For Electronic Users:' \
-                  ' Please adjust box sizes as data is entered.' }
-        ]
       end
     end
   end
